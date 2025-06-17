846dc344622aa1a5a7eeab6ab83455a0227dd8d0
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-06-16 20:49:11 -0400

    Implemente todos los microservicios requeridos.

---
+++ ms-pdi/src/test/java/com/caso_aduana/pdi/PdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caso_aduana.pdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
