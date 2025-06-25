d1bcfbf488dc946a194d0e5de771ab5efe0858a9
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-06-24 20:59:53 -0400

    Finalice modelo de Usuario

--- ms-usuarios/src/main/java/com/caso_aduana/usuario/model/UsuarioModel.java
+++ ms-usuarios/src/main/java/com/caso_aduana/usuario/model/UsuarioModel.java
@@ -1,11 +1,14 @@
 package com.caso_aduana.usuario.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Date;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "usuario")
@@ -18,21 +21,41 @@ public class UsuarioModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUsuario;
 
-    @Size(max = 12)
+    @NotNull
+    @Size(max = 8)
     @Column(name = "run", nullable = false, length = 8)
     private String run;
 
+    @NotNull
     @Column(name = "dv", nullable = false, length = 1)
     private Character dv;
 
+    @NotNull
+    @Size(max = 50)
     @Column(name = "pnombre", nullable = false, length = 50)
     private String pnombre;
 
+    @Size(max = 50)
     @Column(name = "snombre", nullable = true, length = 50)
     private String snombre;
+
+    @NotNull
+    @Size(max = 50)
+    @Column(name = "appaterno", nullable = false, length = 50)
     private String appaterno;
+
+    @Size(max = 50)
+    @Column(name = "apmaterno", nullable = true, length = 50)
     private String apmaterno;
+
+    @NotNull
+    @Email
+    @Size(max = 150)
+    @Column(name = "correo", nullable = false, length = 150)
     private String correo;
-    private Date fecha_nac;
+
+    @NotNull
+    @Column(name = "fecha_nac", nullable = false)
+    private LocalDate fechaNac;
 
 }
