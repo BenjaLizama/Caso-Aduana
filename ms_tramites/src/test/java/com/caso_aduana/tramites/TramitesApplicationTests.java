4e99c0a32ba1b41c569ebf1a040e1a1beadf27c0
nekokawa <lucc.martinez@duocuc.cl>
2025-06-24 21:03:55 -0400

    cree el microservisio tramites para tramites en general

---
+++ ms_tramites/src/test/java/com/caso_aduana/tramites/TramitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caso_aduana.tramites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
